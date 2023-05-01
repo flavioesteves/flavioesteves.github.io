3ffa80794b565051d5296d1760642271c58e1524
Flavio Esteves <flavioafesteves@gmail.com>
2023-05-01 21:32:38 +0100

    upd: theme_switch refactor

--- src/components/theme_switch.rs
+++ src/components/theme_switch.rs
@@ -1,44 +1,55 @@
 use leptos::*;
-//use leptos_meta::{provide_meta_context, Meta, MetaProps};
-use web_sys::{window, Document, Window};
+use wasm_bindgen::JsValue;
+use web_sys::{console, window, Document, Element, Window};
 
 /// Theme Switcher beetween dark and light theme colors
 #[component]
 pub fn ThemeSwitch(cx: Scope) -> impl IntoView {
     //  provide_meta_context(cx);
+    let is_light = get_theme();
 
-    let (prefers_theme, set_prefers_theme) = create_signal(cx, false);
+    console::log_1(&JsValue::from_str(&is_light.to_string()));
+    let (prefers_theme, set_prefers_theme) = create_signal(cx, is_light);
 
-    let change_theme = move |_| set_prefers_theme.update(|dark| *dark = !*dark);
-    let color_scheme = move || {
-        // console_log(format!("{:?}", prefers_theme.get()).as_str());
+    let change_theme = move |_| {
+        set_prefers_theme.update(|dark| *dark = !*dark);
         if prefers_theme.get() {
-            set_scheme("dark");
-            "dark".to_string()
+            set_scheme("dark")
         } else {
-            set_scheme("light");
-            "light".to_string()
+            set_scheme("light")
         }
     };
 
     view! { cx,
-        //<Meta name="color-scheme"  content=color_scheme />
-        <button on:click=change_theme>{color_scheme}</button>
+        <button on:click=change_theme>"Hello"</button>
     }
 }
-pub fn set_scheme(theme: &str) {
+
+fn set_scheme(theme: &str) -> () {
+    let theme_selector = get_document_element();
+    theme_selector.set_attribute("data-theme", &theme);
+    ()
+}
+
+fn get_theme() -> bool {
+    let html_element = get_document_element();
+    let theme = html_element
+        .get_attribute("data-theme")
+        .expect("Data Theme");
+
+    match theme.as_str() {
+        "dark" => true,
+        _ => false,
+    }
+}
+
+fn get_document_element() -> Element {
     let window: Window = window().expect("Window not found!!");
     let document: Document = window.document().expect("Document not found!!");
-
-    let color_schema = document
-        .query_selector("meta[name=\"color-scheme\"]")
-        .expect("color-schema property")
-        .unwrap();
 
     let theme_selector = document
         .document_element()
         .expect("Document element not found");
 
-    theme_selector.set_attribute("data-theme", &theme);
-    color_schema.set_attribute("content", &theme);
+    theme_selector
 }
