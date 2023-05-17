ed3872c9befbd2a7cd036488a17a9f45e34471fb
Flavio Esteves <flavioafesteves@gmail.com>
2023-05-17 15:24:28 +0100

    Upd: contact funcionality to open the predefined email app to sent a email, and mobile styling

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -1,9 +1,9 @@
 use leptos::*;
 
-const RESUME_1: &str = "Hello, my name is Flávio Esteves, and I embarked on my journey as a Software Developer 5 years ago when I enrolled in a full-stack developer Bootcamp focused on Java. Since then, I have been captivated by the opportunity to learn something new every day and apply that knowledge in practical ways.";
-const RESUME_2: &str = "With four years of experience in developing custom applications and working with the ServiceNow Platform, I have now decided to embrace a fresh challenge by returning to my coding roots and moving away from Low-Code environments.";
-const RESUME_3: &str = "I am incredibly excited to tackle new challenges and broaden my skill set. My passion for continuous learning and improvement drives me, and I am actively seeking opportunities to transition into a different technology stack where I can utilize my customer-focused experience to create robust and innovative solutions.";
-const RESUME_4: &str = "During my free time, I thoroughly enjoy exploring new culinary experiences, whether by cooking dishes myself or discovering new restaurants. Additionally, I am an ardent music enthusiast, appreciating a wide range of genres. You can often find me attending concerts as there's nothing quite like the energy of a live performance. 🙂";
+const RESUME_1: &str = "Hello, my name is Flávio Esteves, and I embarked on my journey as a Software Developer five years ago when I enrolled in a full-stack developer Bootcamp focused on Java. Since then, I have been captivated by the opportunity to learn something new every day and apply that knowledge in practical ways.";
+const RESUME_2: &str = "With five years of experience in developing custom applications and working with the ServiceNow Platform, I have now decided to embrace a fresh challenge by returning to my coding roots and moving away from Low-Code environments.";
+const RESUME_3: &str = "I am incredibly excited about this change so I can tackle new challenges and broaden my skill set. My passion for continuous learning and improvement drives me, and I am actively seeking opportunities to transition into a different technology stack where I can utilize my customer-focused experience to create robust and innovative solutions.";
+const RESUME_4: &str = "During my free time, I thoroughly enjoy exploring new culinary experiences, whether by cooking dishes myself or discovering new restaurants. Additionally, I am an ardent music enthusiast, appreciating a wide range of music genres. You can often find me attending concerts as there's nothing quite like the energy of a live performance. 🙂";
 
 #[component]
 pub fn About(cx: Scope) -> impl IntoView {
--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -2,26 +2,20 @@ use leptos::*;
 
 #[component]
 pub fn Contact(cx: Scope) -> impl IntoView {
-    let input_node_name = create_node_ref(cx);
-    let input_node_email = create_node_ref(cx);
     let input_node_subject = create_node_ref(cx);
-
     view! {cx,
     <section id="contact" class="align-right">
         <div class="container">
             <div>
                 <h1>"Contact Me"</h1>
+                <p>"flavioafesteves@gmail.com"</p>
             </div>
-            <form>
-                <label for="name">{"Name: "}</label>
-                <input ref={input_node_name} type="text" id="name" name="name" required=true/>
-                <label for="email">"Email: "</label>
-                <input ref={input_node_email} type="text" id="email" name="email" required=true />
+            <form action="mailto:flavioafesteves@gmail.com" method="GET">
                 <label for="subject">"Subject: "</label>
                 <input ref={input_node_subject} type="text" id="subject" name="subject" required=true/>
-                <label for="message">"Message: "</label>
-                <textarea  type="text" id="message" name="message" required=true/>
-                <button>"Sent"</button>
+                <label for="message_text">"Message: "</label>
+                <textarea  type="text" id="body" name="body" required=true/>
+                <button>"Create Email"</button>
             </form>
         </div>
     </section>
