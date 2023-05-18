1ed133fb5026cffe2c1bf387a8cf16079d38f7d9
flavioesteves <flavioesteves@users.noreply.github.com>
2023-05-18 16:57:35 +0000

    deploy: 6349feb0ce28b11a8ce529fabd732bd5903daf56

--- flavio_esteves_web-69484545adf7d124.js
+++ flavio_esteves_web-69484545adf7d124.js
@@ -829,7 +829,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('flavio_esteves_web-e882ac7384297f90_bg.wasm', import.meta.url);
+        input = new URL('flavio_esteves_web-69484545adf7d124_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
