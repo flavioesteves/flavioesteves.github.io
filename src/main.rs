9ef6f0c03d4c33f3a25529caa7594a92ffd3f742
Flavio Esteves <flavioafesteves@gmail.com>
2023-05-18 21:45:31 +0100

    upd: routing

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,9 @@ pub fn App(cx: Scope) -> impl IntoView {
         <Navbar/>
         <Router>
         <Routes>
-            <Route path=""  view=|cx| view! {cx, <Homepage/>} />
-            <Route path="about" view=|cx| view! {cx, <About />} />
-            <Route path="contact" view=|cx| view! {cx, <Contact />} />
+            <Route path="/"  view=|cx| view! {cx, <Homepage/>} />
+            <Route path="/about" view=|cx| view! {cx, <About />} />
+            <Route path="/contact" view=|cx| view! {cx, <Contact />} />
             <Route path= "*any" view=|cx| view! {cx, <NotFoundPage />} />
         </Routes>
         </Router>
